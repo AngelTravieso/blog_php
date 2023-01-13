d4b64f3098b55871536c1cbf1478bd44b8f5c359
AngelTravieso <angeltraviesoc@gmail.com>
2023-01-12 22:44:57 -0400

    maquetacion blog (terminada)

---
+++ js/index.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// Variables
+const year = new Date().getFullYear();
+
+// Selectores
+const yearSelector = document.querySelector(".year");
+const phantom = document.querySelector(".phantom");
+
+// Funciones
+const cargarEventListeners = () => {
+    // Esperar que el DOM este cargado
+    // document.addEventListener("DOMContentLoaded", () => {
+
+    // });
+}
+
+// Eventos
+//* cargar eventos
+// cargarEventListeners();
+
+
+

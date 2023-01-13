f0d8984eb7c744388c76ae00ac2577ac6523c765
Angel Travieso <angeltraviesoc@gmail.com>
2023-01-13 10:08:05 -0400

    modificaciones interfaz

--- js/index.js
+++ js/index.js
@@ -5,19 +5,7 @@ const year = new Date().getFullYear();
 
 // Selectores
 const yearSelector = document.querySelector(".year");
-const phantom = document.querySelector(".phantom");
 
-// Funciones
-const cargarEventListeners = () => {
-    // Esperar que el DOM este cargado
-    // document.addEventListener("DOMContentLoaded", () => {
-
-    // });
-}
-
-// Eventos
-//* cargar eventos
-// cargarEventListeners();
-
-
+// Imprimir año actual en el footer
+yearSelector.textContent = year;
 
